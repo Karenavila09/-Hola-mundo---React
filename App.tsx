2847baa236794141759145ef92205a0e4d511a5a
Karenavila09 <69640410+Karenavila09@users.noreply.github.com>
2023-03-01 19:00:24 -0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>¡Hola mundo!</h1>
+      <p>🌎 🌍 🌏 </p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# -Hola-mundo---React
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-eg9kdv)
